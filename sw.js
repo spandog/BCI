e9b14b5bcd1567fe7285d2e5ce72e9f6993e111b
spandog <brian.hinojosa@icloud.com>
2026-09-25 03:35:00 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    Everything else (css/js/images) is stale-while-revalidate: served
    from cache first for speed, updated in the background.
    ============================================================ */
-var CACHE_NAME='bci-cache-v167';
+var CACHE_NAME='bci-cache-v168';
 var CORE_ASSETS=[
   '2027.html',
   'index.html',
